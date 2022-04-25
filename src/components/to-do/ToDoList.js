c1e480d1bf82787c68553f20e5cf9dd0386bb118
sanchadr002 <sanchadr002@gmail.com>
2022-04-25 12:23:05 -0700

    create to-do list component

---
+++ src/components/to-do/ToDoList.js
@@ -0,0 +1,13 @@
+const ToDoList = (props) => {
+    console.log('this is props in ToDoList', props)
+    return(
+        <>
+            <div>
+                <h1>Today's To-Dos:</h1>
+                <ul>
+                    <li>task list goes here</li>
+                </ul>
+            </div>
+        </>
+    )
+}
