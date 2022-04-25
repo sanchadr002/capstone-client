d408623bc66217152e63b5bf23521c475ead4246
Nash4star <forrester.nash@gmail.com>
2022-04-25 14:47:52 -0400

    Name Change

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -56,7 +56,7 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                react-auth-template
+                Home Hero
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
