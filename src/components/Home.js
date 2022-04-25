c9a688ba529ef654e74356b92b7ede6dff96e20e
sanchadr002 <sanchadr002@gmail.com>
2022-04-25 11:35:25 -0700

    update Home page with simple user instructions

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,15 @@ const Home = (props) => {
 
 	return (
 		<>
-			<h2>Home Page</h2>
+			<h2>Welcome to the Home Hero app!</h2>
+			<p>Home Hero is a gamified daily to-do list app where users can get in-app coins for completing tasks, and spend those coins on customizing their in-app home area.<br/>
+				Here's how to get started:<br/>
+				Sign-up or log-in,<br/>
+				If it's your first time using Home Hero, you'll have to choose your character and give them a name,<br/>
+				Check in with today's to-do list and check off your completed tasks or create new ones,<br/>
+				Get your coins and get to customizing your character's home area!
+
+			</p>
 		</>
 	)
 }
