2fb5b4cd6f0afcb137c5b8955e0fad37c0461334
sanchadr002 <sanchadr002@gmail.com>
2022-04-27 13:42:24 -0700

    add functional task form

---
+++ src/components/shared/TaskForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+
+const TaskForm = (props) => {
+
+    const {task, handleChange, handleSubmit} = props
+    console.log('this is props in TaskForm', props)
+    return (
+        <>
+            <form 
+                onSubmit={handleSubmit}
+                 
+            >
+                Task title: 
+                    <input 
+                        type="text" 
+                        name='title'
+                        value={task.title}
+                        onChange={handleChange}
+                    />
+                <br/>
+                Task description: 
+                    <input 
+                        type="text" 
+                        name='description'
+                        value={task.description}
+                        onChange={handleChange}
+                    />
+                <br/>
+                Coins: 
+                    <input 
+                        type="number" 
+                        name='coins'
+                        value={task.coins}
+                        onChange={handleChange}
+                    />  
+                <br/>
+                <input 
+                    type="submit" 
+                    value="Create task"    
+                />
+            </form>
+        </>
+    )
+}
+
+export default TaskForm
