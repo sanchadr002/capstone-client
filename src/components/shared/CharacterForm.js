e201f9ac7e3b9df9bce674665f9e5f07ff13ea76
sanchadr002 <sanchadr002@gmail.com>
2022-04-29 17:41:24 -0700

    begin adding code for creating a character with a form

---
+++ src/components/shared/CharacterForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const CharacterForm = (props) => {
+
+    const {character, handleChange, handleSubmit} = props
+    console.log('this is props in CharacterForm', props)
+    return (
+        <>
+            <form 
+                onSubmit={handleSubmit}
+                 
+            >
+                Character name: 
+                    <input 
+                        type="text" 
+                        name='name'
+                        value={character.name}
+                        onChange={handleChange}
+                    />
+                <br/>
+                Character Job: 
+                    <input 
+                        type="text" 
+                        name='job'
+                        value={character.job}
+                        onChange={handleChange}
+                    />
+                <br/>
+                <input 
+                    type="submit" 
+                    value="Create character"    
+                />
+            </form>
+        </>
+    )
+}
+
+export default CharacterForm
