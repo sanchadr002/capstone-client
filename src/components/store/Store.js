f1a335e7b8b51784078b5c5c90b9be47a8437d71
Nash4star <forrester.nash@gmail.com>
2022-05-01 17:10:59 -0400

    Set up for pushing items from store to Character

--- src/components/store/Store.js
+++ src/components/store/Store.js
@@ -1,20 +1,16 @@
 import React, { useState, useEffect} from 'react'
 import { Spinner, Container } from 'react-bootstrap'
-import {getStore} from '../../api/store'
-
+import {getStore, addItem, updateItem} from '../../api/store'
 // const cardContainerLayout = {
 //     display: 'flex',
 //     justifyContent: 'center',
 //     flexFlow: 'row wrap'
 // }
-
 const Store = (props) => {
-
     const {user} = props
     const [store, setStore] = useState(null)
     console.log('this is props in game page', props)
     console.log('this is user.playerStore', user.playerStore)
-
     let storeIndex
     if (user.playerStore.inventory) {
         // petsJsx = pets.map(pet => (
@@ -27,34 +23,34 @@ const Store = (props) => {
             // one method of styling, usually reserved for a single style
             // we can use inline, just like in html
             console.log('this is the item in Store', item)
-            // const handleClick = (e) => {
+            const handleClick = (e) => {
             //     // e === event
-            //     e.preventDefault()
-            //     //access task.coins 
+                e.preventDefault()
+            //     //access task.coins
             //     user.playerCharacter.coins += task.coins
+                console.log('this is user.playerCharacter.coins pre-transaction', user.playerCharacter.coins)
+                user.playerCharacter.coins -= item.item.cost
             //     task.coins -= task.coins
-            //     console.log('this is user.playerCharacter.coins', user.playerCharacter.coins)
+                console.log('this is user.playerCharacter.coins post-transaction', user.playerCharacter.coins)
             //     // .then((character) => {
             //     //     character.coins += task.coins
             //     //     task.coins -= task.coins
             //     // })
             //     //push task.coins to user.coins
             //     //subtract task.coins from itself
-            // }
-
+            }
             return(
                 <>
                     <form>
                         <header>Description: {item.item.description}</header>
                         <li>Cost: {item.item.cost}</li>
-                        <button name='buy'>Buy</button>
+                        <button onClick={handleClick} name='buy'>Buy</button>
                     </form>
                 </>
             )
         })
         console.log('this is storeIndex', storeIndex)
     }
-
     if (!user.playerStore.inventory) {
         return (
             <Container fluid className="justify-content-center">
@@ -64,13 +60,10 @@ const Store = (props) => {
             </Container>
         )
     }
-
-
     return(
         <>
             {storeIndex}
         </>
     )
 }
-
 export default Store
