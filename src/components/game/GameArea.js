878d6d3a8a69f54e4061c18ee85fec3e0cedbbea
jscodeguy <im.still.at.school99@gmail.com>
2022-05-02 14:55:51 -0400

    cleaning up character page

--- src/components/game/GameArea.js
+++ src/components/game/GameArea.js
@@ -1,55 +1,14 @@
 import React, { useState, useEffect} from 'react'
 import { Spinner, Container } from 'react-bootstrap'
-import Store from '../store/Store'
 import mans from '../../srcImg/mans.png'
-// const cardContainerLayout = {
-//     display: 'flex',
-//     justifyContent: 'center',
-//     flexFlow: 'row wrap'
-// }
-
-// test again
 
 const GameArea = (props) => {
-
     const {user} = props
     const [character, setCharacter] = useState(user.playerCharacter)
     console.log('this is props in game page', props)
     useEffect(() => {
         setCharacter(user.playerCharacter)
-        // .then(() => {
-        //     console.log('this is character', character)
-        // })
-        // .catch((err) => console.log(err))
     })
-
-    // useEffect(() => {
-    //     getTheCharacter(user)
-    //     .then(res => {setChar(res.data.character)
-    //         console.log('this is character', character)
-    //     })
-        
-    //     .catch((error) => {
-    //     })
-    // }, [updated])
-
-    // if (character.ownedItems){
-    //     itemIndex = character.ownedItems.map(item => {
-    //         return(
-    //             <li>{item}</li>
-    //             )
-                
-    //         })
-    //     }
-        
-    //     if (!character.ownedItems){
-    //         return(
-    //             <>
-    //             loading items
-    //         </>
-    //     )
-    // }
-    
     if (character){
         let itemIndex
         console.log('this is character in GameArea', character)
@@ -72,7 +31,6 @@ const GameArea = (props) => {
             </div>
         )
     }
-
     if (!character) {
         return (
             <Container fluid className="justify-content-center">
@@ -82,20 +40,14 @@ const GameArea = (props) => {
             </Container>
         )
     }
-
     return(
         <>
             <div>
                 GamePage
                 <p>{character}</p>
             </div>
-            <div>
-                store
-                {/* <Store user={user}/> */}
-            </div>
         </>
     )
 }
 
 export default GameArea
-//this is to fix shanes code
