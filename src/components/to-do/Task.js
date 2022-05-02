97afbc68326687e4dea1d4513192c2b7a539afd3
jscodeguy <im.still.at.school99@gmail.com>
2022-05-02 15:02:34 -0400

    cleaning up task component

--- src/components/to-do/Task.js
+++ src/components/to-do/Task.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import {getAllTasks} from '../../api/task'
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { getOneTask } from '../../api/task'
+import { useNavigate } from 'react-router-dom'
 import { patchCharacter } from '../../api/character'
 import { completeTask, deleteTask } from '../../api/task'
 
 const Task = (props) => {
-    const { state } = useLocation()
     const [tasks, setTasks] = useState(null)
-    const [show, setShow] = useState(true)
     const { user } = props
     const [updated, setUpdated] = useState(false)
     const navigate = useNavigate()
@@ -20,19 +17,9 @@ const Task = (props) => {
                 console.log('this is tasks', tasks)
             })
             .then(() =>
-                // msgAlert({
-                //     heading: 'Success!',
-                //     message: `Favorites shown!`,
-                //     variant: 'success',
-                // })
                 console.log('tasks displayed')
             )
             .catch(()=> {
-                // msgAlert({
-                //     heading: 'Oh No!',
-                //     message: 'Issue with retrieving favorites',
-                //     variant: 'danger',
-                // })
                 console.log('tasks not displayed')
             })
         }, 
@@ -44,15 +31,9 @@ const Task = (props) => {
         return <p>no tasks yet, go add some</p>
     }
 
-
     let taskIndex
 
     if (tasks.length > 0) {
-        // petsJsx = pets.map(pet => (
-        //     <li key={pet.id}>
-        //         {pet.fullTitle}
-        //     </li>
-        // ))
         taskIndex = tasks.map(task => {
             // one method of styling, usually reserved for a single style
             // we can use inline, just like in html
@@ -63,26 +44,19 @@ const Task = (props) => {
                 e.preventDefault()
                 //access task.coins
                 completeTask(user,task)
-                .then(() => {navigate(`/todolist`)})
-                .catch(() => {
+                    .then(() => {navigate(`/todolist`)})
+                    .catch(() => {
                     console.log('no update')
                 })
                 user.playerCharacter.coins += task.coins
                 task.coins -= task.coins
                 patchCharacter(user, user.playerCharacter)
-                .then(() => {
-                    console.log('---coins added---')
-                })
-                .catch(err => console.log(err))
+                    .then(() => {
+                        console.log('---coins added---')
+                    })
+                    .catch(err => console.log(err))
                 console.log('this is user.playerCharacter.coins', user.playerCharacter.coins)
                 console.log('this is if the task is completed or not', task.completed)
-                // .then((character) => {
-                //     character.coins += task.coins
-                //     task.coins -= task.coins
-                // })
-                //push task.coins to user.coins
-                //subtract task.coins from itself
-                // const handleStyle = setShow((s) => !s)
             }
             const handleDelete = (e) => {
                 e.preventDefault()
@@ -96,11 +70,10 @@ const Task = (props) => {
                 <>
                     <form key={task._id}>
                         <header>Title: {task.title}</header>
-                        <li>Description: {task.description}</li>
-                        <li style={{ display: task.completed ? "none": "block" }}>Coins: {task.coins}</li>
-                        <li style={{ display: task.completed ? "block": "none" }}>Task is complete!</li>
+                            <li>Description: {task.description}</li>
+                            <li style={{ display: task.completed ? "none": "block" }}>Coins: {task.coins}</li>
+                            <li style={{ display: task.completed ? "block": "none" }}>Task is complete!</li>
                         <button  
-                            // style={{ display: show ? "inline-block" : "none" }} 
                             style={{ display: task.completed ? "none": "inline-block" }} 
                             onClick={handleClick} 
                             name='completed'
@@ -127,4 +100,3 @@ const Task = (props) => {
 }
 
 export default Task
-//this is to fix shanes code
